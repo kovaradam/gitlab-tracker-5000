73536eeccddadbd06c04de0a352bb14f8dd5c1be
Adam Kovar <kovaradam@pop-os.localdomain>
2021-11-13 17:10:33 +0100

    :recycle: update env

--- server/src/deps.ts
+++ server/src/deps.ts
@@ -1,4 +1,3 @@
-export * as dotenv from 'https://deno.land/x/dotenv@v3.0.0/mod.ts';
 export * as asserts from 'https://deno.land/std@0.109.0/testing/asserts.ts';
 export * as oak from 'https://deno.land/x/oak@v9.0.1/mod.ts';
 export * as cors from 'https://deno.land/x/cors/mod.ts';
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
+import 'https://deno.land/x/dotenv/load.ts';
 import { auth, getToken } from './auth.ts';
-import { dotenv, oak, cors } from './deps.ts';
+import { oak, cors } from './deps.ts';
 import * as Storage from './lib/storage.ts';
 import { createTimestamp } from './utils.ts';
 
@@ -7,10 +8,7 @@ const storage = Deno.args?.includes('--location')
   ? Storage.createLocalStorage()
   : Storage.createInMemoryStorage();
 
-const env = dotenv.config();
-
-const hostname = env.HOSTNAME ?? '0.0.0.0';
-const port = env.PORT ? Number(env.PORT) : 3001;
+const port = Number(Deno.env.get('PORT')) ?? 8080;
 
 const [app, router] = [new oak.Application(), new oak.Router()];
 
@@ -56,5 +54,5 @@ router.delete('/api/timestamp', (context) => {
 });
 
 app.use(router.routes());
-app.listen({ hostname, port });
-console.info(`Running at http://${hostname}:${port}/`);
+app.listen({ port });
+console.info(`Running on port ${port}`);
